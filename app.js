dd03ac629595f9c52d00c6f92fe5f0f1ebbddc37
ogbon <onethinneedful@gmail.com>
2021-01-06 16:46:58 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+exports.findAverage = (...numbers) => {
+  
+    if(numbers.length < 1) {
+      throw new Error("This function expects at least one argument");
+    }
+    if(!numbers.every((element) => Number.isInteger(element))) {
+      throw new Errow("The function expects an integer");
+    }
+    
+    const average = numbers.reduce((a,b) => a + b, 0) / numbers.length;
+    
+    return Number.isInteger(average) ? average : parseFloat(average.toFixed(2));
+}
